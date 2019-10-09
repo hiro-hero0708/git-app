917a5e9b7051d1f42ef149da53f1b9212cba94ef
hiro-hero0708 <hiroto435043@gmail.com>
2019-10-09 16:43:38 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @articles = Article.all
-  end
-
-  def show
-  end
-
-  def new
-    @article = Article.new
-  end
 
   # GET /articles/1/edit
-  def edit
-  end
+
 
   # POST /articles
   # POST /articles.json
